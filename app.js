463c61ae6f929e9756a220de8e913b9fc42fa30a
Tomás Mier <68940061+onlysunshines@users.noreply.github.com>
2020-11-25 09:30:56 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,52 @@
 const express = require('express');
 const morgan = require('morgan');
-const cors = require('cors');
-const playApps = require('./playstore.js');
 
 const app = express();
 
-app.use(morgan('common'));
-app.use(cors());
+app.use(morgan('dev'));
+
+const apps = require('./playstore');
 
 app.get('/apps', (req, res) => {
-    const { genres = "", sort } = req.query;
+  let { sort, genres } = req.query;
+  let results = [...apps];
 
-    if(sort) {
-        if(!['rating', 'app'].includes(sort)) {
-            return res
-                .status(400)
-                .send('Sort by "rating" or "app"');
-        }
-    }
+  if (sort && !['rating', 'app'].includes(sort.toLowerCase())) {
+    return res.send(`Sort should be 'rating' or 'app'`);
+  }
 
-    let results = playApps
-        .filter(playApp =>
-            playApp
-                .Rating
-                );
+  if (sort) {
+    sort = sort.toLowerCase();
+    sort = sort[0].toUpperCase() + sort.slice(1, sort.length);
+    results.sort((currVal, nextVal) => {
+      let a = currVal[sort];
+      let b = nextVal[sort];
+      if (typeof currVal[sort] === 'string') {
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+      }
+      return a < b ? -1 : a > b ? 1 : 0;
+    });
+  }
 
-    if(sort) {
-        results
-            .sort((a, b) => {
-                return a[sort] > b[sort] ? 1 : a[sort] < b[sort] ? -1 : 0;
-            })
-    }
+  if (genres) {
+    if (
+      !['action', 'puzzle', 'strategy', 'casual', 'arcade', 'card'].includes(
+        genres.toLowerCase()
+      )
+    )
+      return res.send(
+        'You should choose one of these genres: Action, Puzzle, Strategy, Casual, Arcade or Card'
+      );
 
-    res
-        .json(results);
+    genres = genres.toLowerCase();
+    genres = genres[0].toUpperCase() + genres.slice(1, genres.length);
+    results = results.filter((result) => result.Genres.includes(genres));
+  }
+  res.json(results);
+});
 
-})
-
-app.listen(8000, () => {
-    console.log('Server started on Port 8000');
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log(`App is running in port:${PORT}`);
 });
